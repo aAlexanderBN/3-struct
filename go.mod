e273e14da6c10eb43a8004b7976e02ce1ac35aea
Alexander <aalexanderbn@icloud.com>
2025-11-05 01:50:08 +0300

    создал структуру, слайс и функцию создания

---
+++ go.mod
@@ -0,0 +1,3 @@
+module my-project/3-struct
+
+go 1.25.3
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type BinList []*Bin
+
+type Bin struct {
+	Id       string
+	Private  bool
+	CreateAt time.Time
+	Name     string
+}
+
+func main() {
+
+	b1 := NewBin("1", true, time.Now(), "test")
+	b2 := NewBin("2", false, time.Now(), "test2")
+	var blist BinList
+	blist = append(blist, b1, b2)
+	fmt.Println(*blist[0], *blist[1])
+	fmt.Println(blist)
+}
+
+func NewBin(id string, private bool, createAt time.Time, name string) *Bin {
+
+	return &Bin{
+		Id:       id,
+		Private:  private,
+		CreateAt: createAt,
+		Name:     name,
+	}
+
+}

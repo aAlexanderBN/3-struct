2a9c0c5afb57ae21249335290422a3e2becea68f
Alexander <aalexanderbn@icloud.com>
2025-11-05 14:55:26 +0300

    Добавил пакеты api, file , storage, bins в bins перенес структуры и фукнции связанные с ним

---
+++ bins/bins.go
@@ -0,0 +1,22 @@
+package bins
+
+import "time"
+
+type BinList []*Bin
+
+type Bin struct {
+	Id       string
+	Private  bool
+	CreateAt time.Time
+	Name     string
+}
+
+func NewBin(id string, private bool, createAt time.Time, name string) *Bin {
+
+	return &Bin{
+		Id:       id,
+		Private:  private,
+		CreateAt: createAt,
+		Name:     name,
+	}
+}

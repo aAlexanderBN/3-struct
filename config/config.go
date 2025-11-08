1298c7ae9108716a36ad6b69df652ab7d037b6ca
Alexander <aalexanderbn@icloud.com>
2025-11-09 00:06:33 +0300

    добавил пакет config и чтение ключа из .env

---
+++ config/config.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Key string
+}
+
+func New() *Config {
+	err := godotenv.Load("config/.env")
+	if err != nil {
+		log.Fatal("❌ Не удалось загрузить .env файл:", err)
+	}
+	return &Config{
+		Key: os.Getenv("DATABASE_URL"),
+	}
+
+}
